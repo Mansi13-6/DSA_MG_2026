e4b86c0e568ea84aaccd779bfb8ad235b212f162
Mansi13-6 <mansigngrd136@gmail.com>
2026-04-05 17:36:21 +0530

    Updated the assignments of Singly linked list

---
+++ Assignments/DSA_MG_2026/02-SinglyLinkedList/03-alternate.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct singlyLL
+{
+    int data;
+    char name[20];  
+    struct singlyLL *next;
+}sl;
+
+sl* create(sl *head);
+void displayAlternate(sl *head);
+
+int main()
+{
+    sl *head = NULL;
+    head = create(head);
+    displayAlternate(head);
+    return 0;
+}
+
+sl* create(sl *head) 
+{
+    sl *temp, *last = NULL;
+    int i;
+    for(i = 0; i < 3; i++)
+    {
+        temp = (sl*)malloc(sizeof(sl));
+        printf("Enter the data and name: ");
+        scanf("%d %s", &temp->data, temp->name);
+        temp->next = NULL;
+        if(head == NULL)
+        {
+            head = temp;
+            last = temp;
+        } 
+        else 
+        {
+            last->next = temp;
+            last = temp;
+        }
+    }
+    return head;
+}
+
+void displayAlternate(sl *head)
+{
+    sl *p = head;
+    printf("\nAlternate nodes are:\n");
+    while(p != NULL)
+    {
+        printf("Data: %d, Name: %s\n", p->data, p->name);
+        if(p->next != NULL)
+            p = p->next->next;
+        else
+            break;
+    }
+}
