b385fe13a4871eed4087e8fd549c0057bfa36faa
Mansi13-6 <mansigngrd136@gmail.com>
2026-05-29 20:16:32 +0530

    updated

---
+++ Assignments/04-SinglyCircularLinkedList/Assignment12/12-delete_after_key.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+    int data;
+    char name[20];
+    struct node *next;
+
+} node_t;
+
+node_t *head = NULL;
+
+void create(int data, char name[])
+{
+    node_t *newnode, *temp;
+
+    newnode = (node_t *)malloc(sizeof(node_t));
+
+    newnode->data = data;
+    strcpy(newnode->name, name);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        newnode->next = head;
+    }
+    else
+    {
+        temp = head;
+
+        while (temp->next != head)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newnode;
+        newnode->next = head;
+    }
+}
+
+void delete_after_key(int key)
+{
+    node_t *temp, *del;
+
+    temp = head;
+
+    do
+    {
+        if (temp->data == key)
+        {
+            del = temp->next;
+
+            if (del == head)
+            {
+                return;
+            }
+
+            temp->next = del->next;
+
+            free(del);
+
+            return;
+        }
+
+        temp = temp->next;
+
+    } while (temp != head);
+}
+
+void display()
+{
+    node_t *temp;
+
+    temp = head;
+
+    do
+    {
+        printf("|_%d_|_%s_| --> ",
+               temp->data,
+               temp->name);
+
+        temp = temp->next;
+
+    } while (temp != head);
+
+    printf("HEAD\n");
+}
+
+int main()
+{
+    int key;
+
+    create(1, "AA");
+    create(2, "BB");
+    create(3, "CC");
+    create(4, "DD");
+
+    display();
+
+    printf("Enter key : ");
+    scanf("%d", &key);
+
+    delete_after_key(key);
+
+    display();
+
+    return 0;
+}
